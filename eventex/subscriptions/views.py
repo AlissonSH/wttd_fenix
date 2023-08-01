7cc9cbbefec45531a6610d91d147e9f66b2de8c2
AlissonSH <alisson_sielo@hotmail.com>
2023-07-31 23:27:16 -0300

    From alinhado com o gmail identity

--- eventex/subscriptions/views.py
+++ eventex/subscriptions/views.py
@@ -15,8 +15,8 @@ def subscribe(request):
                                     form.cleaned_data)
             mail.send_mail('Confirmação de Inscrição',
                            body,
-                           'contato@eventex.com.br',
-                           ['contato@eventex.com.br', form.cleaned_data['email']])
+                           'alissonsieloholkem@gmail.com',
+                           ['alissonsieloholkem@gmail.com', form.cleaned_data['email']])
 
             messages.success(request, 'Inscrição realizada com sucesso!')
 
