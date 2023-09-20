b71dc2f1fc99c37a1c17a9922d2b60e216a31e64
AlissonSH <alisson_sielo@hotmail.com>
2023-09-20 10:32:39 -0300

    Persiste a Inscricao

---
+++ eventex/subscriptions/tests/test_models_subscription.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+
+from django.test import TestCase
+from eventex.subscriptions.models import Subscription
+
+
+class SubscriptionModelTest(TestCase):
+    def setUp(self):
+        self.obj = Subscription(
+            name='Álisson Sielo Holkem',
+            cpf='12345678901',
+            email='alissonsieloholkem@gmail.com',
+            phone='55-99206-7827'
+        )
+        self.obj.save()
+
+    def test_create(self):
+        self.assertTrue(Subscription.objects.exists())
+
+    def test_created_at(self):
+        """Subscription must have an auto created_at attr."""
+        self.assertIsInstance(self.obj.created_at, datetime)
--- eventex/subscriptions/views.py
+++ eventex/subscriptions/views.py
@@ -5,6 +5,7 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.template.loader import render_to_string
 from eventex.subscriptions.forms import SubscriptionForm
+from eventex.subscriptions.models import Subscription
 
 
 def subscribe(request):
@@ -21,12 +22,15 @@ def create(request):
         return render(request, 'subscriptions/subscription_form.html',
                       {'form': form})
 
-    _send_mail('Confirmação de Inscrição',
-                settings.DEFAULT_FROM_EMAIL,
-                form.cleaned_data['email'],
-                'subscriptions/subscription_email.txt',
-                form.cleaned_data)
+    subscriber_email = form.cleaned_data['email']
 
+    _send_mail('Confirmação de Inscrição',
+               settings.DEFAULT_FROM_EMAIL,
+               subscriber_email,
+               'subscriptions/subscription_email.txt',
+               form.cleaned_data)
+
+    Subscription.objects.create(**form.cleaned_data)
     messages.success(request, 'Inscrição realizada com sucesso!')
 
     return HttpResponseRedirect('/inscricao/')
