c2ae75deb5e111943e980197c32b8439fb5c918c
AlissonSH <alisson_sielo@hotmail.com>
2024-06-25 11:33:27 -0300

    Update: Ajuste inicial do urls da matricula

--- eventex/urls.py
+++ eventex/urls.py
@@ -5,7 +5,7 @@ from eventex.core.views import home, speaker_detail, talk_list
 urlpatterns = [
     path('', home, name='home'),
     path('inscricao/', include('eventex.subscriptions.urls', namespace='subscriptions')),
-    path('cadastro/', include('eventex.core.urls')),
+    path('matricula/', include('eventex.core.urls')),
     path('palestras/', talk_list, name='talk_list'),
     path('palestrantes/<slug:slug>/', speaker_detail, name='speaker_detail'),
     path('admin/', admin.site.urls),
