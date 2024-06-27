fcc58dd2f7600069f651b1c3fff4449eafb5ea20
AlissonSH <alisson_sielo@hotmail.com>
2024-06-26 23:31:29 -0300

    Update: Criacao de testes para nova url de criacao

--- eventex/core/tests/test_view_create.py
+++ eventex/core/tests/test_view_create.py
@@ -12,3 +12,18 @@ class MatriculaListView(TestCase):
 
     def test_template(self):
         self.assertTemplateUsed(self.resp, 'core/matricula_list.html')
+
+    def test_creates_link(self):
+        expected = 'href="{}"'.format(r('core:matricula_create'))
+        self.assertContains(self.resp, expected)
+
+
+class MatriculaCreateView(TestCase):
+    def setUp(self):
+        self.resp = self.client.get(r('core:matricula_create'))
+
+    def test_get(self):
+        self.assertEqual(200, self.resp.status_code)
+
+    def test_template(self):
+        self.assertTemplateUsed(self.resp, 'core/matricula_create.html')
