2db1b132f08e19f695bb3b057a6189b27a6853b5
AlissonSH <alisson_sielo@hotmail.com>
2024-08-01 15:59:25 -0300

    Update: Testes para as urls e templates da nova app

--- eventex/registration/tests/test_view_create.py
+++ eventex/registration/tests/test_view_create.py
@@ -4,26 +4,26 @@ from django.shortcuts import resolve_url as r
 
 class MatriculaListView(TestCase):
     def setUp(self):
-        self.resp = self.client.get(r('core:matricula_list'))
+        self.resp = self.client.get(r('registration:matricula_list'))
 
     def test_get(self):
         """Get /create/ must return status code 200"""
         self.assertEqual(200, self.resp.status_code)
 
     def test_template(self):
-        self.assertTemplateUsed(self.resp, 'core/matricula_list.html')
+        self.assertTemplateUsed(self.resp, 'registration/matricula_list.html')
 
     def test_creates_link(self):
-        expected = 'href="{}"'.format(r('core:matricula_create'))
+        expected = 'href="{}"'.format(r('registration:matricula_create'))
         self.assertContains(self.resp, expected)
 
 
 class MatriculaCreateView(TestCase):
     def setUp(self):
-        self.resp = self.client.get(r('core:matricula_create'))
+        self.resp = self.client.get(r('registration:matricula_create'))
 
     def test_get(self):
         self.assertEqual(200, self.resp.status_code)
 
     def test_template(self):
-        self.assertTemplateUsed(self.resp, 'core/matricula_create.html')
+        self.assertTemplateUsed(self.resp, 'registration/matricula_create.html')
