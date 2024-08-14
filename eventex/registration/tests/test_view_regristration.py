5950685cffcd7ce1fec3e7d3c022e3fb6a6668d7
AlissonSH <alisson_sielo@hotmail.com>
2024-08-14 17:00:33 -0300

    Update: Testes da view com os valores dela

--- eventex/registration/tests/test_view_regristration.py
+++ eventex/registration/tests/test_view_regristration.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from django.shortcuts import resolve_url as r
+from eventex.registration.forms import RegistrationForm
 
 
 class MatriculaListView(TestCase):
@@ -27,3 +28,14 @@ class MatriculaCreateView(TestCase):
 
     def test_template(self):
         self.assertTemplateUsed(self.resp, 'registration/matricula_create.html')
+
+    def test_csrf(self):
+        self.assertContains(self.resp, 'csrfmiddlewaretoken')
+
+    def test_has_form(self):
+        form = self.resp.context['form']
+        self.assertIsInstance(form, RegistrationForm)
+
+    def test_form_has_fields(self):
+        form = self.resp.context['form']
+        self.assertSequenceEqual(['student', 'cpf', 'phone', 'observation'], list(form.fields))
