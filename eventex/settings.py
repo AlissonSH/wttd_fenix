fe6dcba9970021fa733cfe483c05f9feb6c054c1
AlissonSH <alisson_sielo@hotmail.com>
2024-08-22 17:51:16 -0300

    Update: Registro no settings do rest_framework para utilizar api

--- eventex/settings.py
+++ eventex/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'test_without_migrations',
     'django_extensions',
+    'rest_framework',
     'eventex.core',
     'eventex.registration.apps.RegistrationConfig',
     'eventex.subscriptions.apps.SubscriptionsConfig',
