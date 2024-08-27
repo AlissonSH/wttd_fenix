7d6d843bf2757d4b466d929337da7b4c00d651aa
AlissonSH <alisson_sielo@hotmail.com>
2024-08-27 14:53:49 -0300

    Update: Testes do endpoint para minha url e execucao da funcao do select2

--- eventex/registration/api/registration/test_api_view.py
+++ eventex/registration/api/registration/test_api_view.py
@@ -1,8 +1,10 @@
 from rest_framework.test import APITestCase
 from django.urls import reverse as r
+from eventex.registration.models import Registration
+from eventex.subscriptions.models import Subscription
 
 
-class RegistrationViewSetTestCase(APITestCase):
+class RegistrationViewSetSelect2TestCase(APITestCase):
     def setUp(self):
         self.url = r('registration:student-select2')
         self.resp = self.client.get(self.url)
@@ -14,7 +16,28 @@ class RegistrationViewSetTestCase(APITestCase):
         self.assertEqual(200, self.resp.status_code)
         self.assertEqual(self.resp.json(), {"results": []})
 
-    def test_request_get_with_term(self):
+    def test_request_get_with_term_empty(self):
         query = self.client.get(self.url, {'term': ''})
         self.assertEqual(200, query.status_code)
         self.assertEqual(query.json(), {"results": []})
+
+    def test_request_get_with_invalid_term(self):
+        student_name = 'Alisson Sielo Holkem'
+        student= Subscription.objects.create(name=student_name)
+        Registration.objects.create(student=student)
+
+        query = self.client.get(self.url, {'term': 'Pedro'})
+        self.assertEqual(200, query.status_code)
+        self.assertEqual(query.json(), {"results": []})
+
+    def test_request_get_with_valid_term(self):
+        student_name = 'Alisson Sielo Holkem'
+        student = Subscription.objects.create(name=student_name)
+        Registration.objects.create(student=student)
+
+        query = self.client.get(self.url, {'term': 'Alisson Sielo Holkem'})
+        self.assertEqual(200, query.status_code)
+
+        results = query.json().get('results', [])
+        self.assertGreater(len(results), 0)
+        self.assertIn(student_name, [results['text'] for results in results])
