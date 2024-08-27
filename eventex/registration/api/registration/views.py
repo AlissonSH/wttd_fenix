439dfedd03a50cf4ecd72eeb9505ac42c05ab7fe
AlissonSH <alisson_sielo@hotmail.com>
2024-08-27 14:55:25 -0300

    Update: Implementacao do meu select2 na view do endpoint

--- eventex/registration/api/registration/views.py
+++ eventex/registration/api/registration/views.py
@@ -1,27 +1,21 @@
 from django.http import JsonResponse
 from rest_framework import viewsets
 from rest_framework.decorators import action
-from rest_framework.response import Response
-
-from eventex.registration.models import Registration
 from eventex.subscriptions.models import Subscription
 
 
 class StudentViewSet(viewsets.GenericViewSet):
     @action(detail=False, methods=["get"])
     def select2(self, request):
-
         query = self.request.GET.get('term')
-        print(query)
         if not query or query == '':
             return JsonResponse({"results": []})
 
         inscritos = Subscription.objects.filter(name__icontains=query)
-
+        # criar uma função para pesquisar sem os acentos (REGEX)
         results = {
             "results": [
                 {"id": i.pk, "text": i.name} for i in inscritos
             ]
         }
-
         return JsonResponse(results)
