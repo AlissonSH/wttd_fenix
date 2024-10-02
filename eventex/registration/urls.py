fdc7511c4f3dd2e571ffbaeea4079c9e825790b2
AlissonSH <alisson_sielo@hotmail.com>
2024-10-02 10:31:08 -0300

    Update: Ajuste no chamamento da minha url para a view

--- eventex/registration/urls.py
+++ eventex/registration/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from eventex.registration.api.registration.views import StudentViewSet
-
-from eventex.registration.models import Registration
-from eventex.registration.views import matricula_list, matricula_create
+from eventex.registration.views import MatriculaListView, MatriculaCreateView
 
 app_name = 'registration'
 router = DefaultRouter()
@@ -12,6 +10,6 @@ router.register(r'students', StudentViewSet, basename='student')
 urlpatterns = [
     path('api/', include(router.urls)),
 
-    path('listar/', matricula_list, name='matricula_list'),
-    path('create/', matricula_create, name='matricula_create'),
+    path('listar/', MatriculaListView.as_view(), name='matricula_list'),
+    path('create/', MatriculaCreateView.as_view(), name='matricula_create'),
 ]
