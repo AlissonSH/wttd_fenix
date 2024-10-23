f76ec6bc41233004d174b0b3b94737a526dd8140
AlissonSH <alisson_sielo@hotmail.com>
2024-10-23 17:15:54 -0300

    Update: Teste para novos campos da palestra

--- eventex/registration/tests/test_model_regristration.py
+++ eventex/registration/tests/test_model_regristration.py
@@ -1,4 +1,6 @@
 from django.test import TestCase
+
+from eventex.core.models import Talk
 from eventex.registration.models import Registration
 from eventex.subscriptions.models import Subscription
 
@@ -7,14 +9,19 @@ class RegistrationModelTestCase(TestCase):
     def setUp(self):
         self.students = Subscription.objects.create(
             name="Alisson Sielo Holkem",
-            cpf="12345678901",
-            email="alissonsieloholkem@gmail.com",
-            phone="55-99206-7827"
+            cpf="12345678901"
         )
+        self.talks = Talk.objects.create(
+            title="Python Brasil",
+        )
+
         self.registration = Registration.objects.create(
             student=self.students,
             cpf=self.students.cpf,
             phone="55-99206-7827",
+            talk=self.talks,
+            name_speaker="Henrique Bastos",
+            start_time="08:30",
             observation="Aqui posso escrever algo."
         )
 
@@ -24,7 +31,10 @@ class RegistrationModelTestCase(TestCase):
     def test_has_registration(self):
         self.assertEqual(self.registration.student, self.students)
         self.assertEqual(self.registration.cpf, self.students.cpf)
-        self.assertEqual(self.registration.phone, self.students.phone)
+        self.assertEqual(self.registration.phone, "55-99206-7827")
+        self.assertEqual(self.registration.talk, self.talks)
+        self.assertEqual(self.registration.name_speaker, "Henrique Bastos")
+        self.assertEqual(self.registration.start_time, "08:30")
         self.assertEqual(self.registration.observation, "Aqui posso escrever algo.")
 
     def test_str(self):
