2487ce64838b5199418b0b0ff33db64018aa9ecc
AlissonSH <alisson_sielo@hotmail.com>
2024-10-24 17:01:31 -0300

    Update: Adicao do filtro para as palestras

--- eventex/registration/forms.py
+++ eventex/registration/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+
+from eventex.core.models import Talk, Course
 from eventex.registration.models import Registration
 
 
@@ -6,3 +8,7 @@ class RegistrationForm(forms.ModelForm):
     class Meta:
         model = Registration
         fields = ["student", "cpf", "phone", "talk", "name_speaker", "start_time", "observation"]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["talk"].queryset = Talk.objects.exclude(pk__in=Course.objects.values_list('pk', flat=True))
