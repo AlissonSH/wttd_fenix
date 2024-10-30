49ab6ee0d0406f150f4dbf52e3421e5e2cd774ef
AlissonSH <alisson_sielo@hotmail.com>
2024-10-30 15:50:41 -0300

    Update: Adicao de campo cursos em registration

--- eventex/registration/models.py
+++ eventex/registration/models.py
@@ -9,6 +9,9 @@ class Registration(models.Model):
     talk = models.ForeignKey(
         'core.Talk', verbose_name='Nome da Palestra', null=True, blank=True,
         on_delete=models.PROTECT, related_name='registrations_as_talks')
+    course = models.ForeignKey(
+        'core.Course', verbose_name='Nome do Curso', null=True, blank=True,
+        on_delete=models.PROTECT, related_name='registrations_as_courses')
     name_speaker = models.CharField('Nome do Palestrante', max_length=255, null=True, blank=True)
     start_time = models.TimeField('Horário de Início', null=True, blank=True)
     observation = models.CharField('Observação', max_length=2500, blank=True)
