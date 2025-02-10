c92d1e9a0afa1e4132540a5cd66d193e181b31e0
AlissonSH <alisson_sielo@hotmail.com>
2025-02-10 15:35:11 -0300

    Update: Adicao e separacao de itens talk e itens course no form de registration

--- eventex/registration/forms.py
+++ eventex/registration/forms.py
@@ -1,14 +1,45 @@
 from django import forms
+from django.forms import inlineformset_factory
 
-from eventex.core.models import Talk, Course
-from eventex.registration.models import Registration
+from eventex.core.models import Talk
+from eventex.registration.models import Registration, ItensTalk, ItensCourse
 
 
 class RegistrationForm(forms.ModelForm):
     class Meta:
         model = Registration
-        fields = ["student", "cpf", "phone", "talk", "course", "name_speaker", "start_time", "observation"]
+        fields = ["student", "cpf", "phone", "observation"]
+
+
+class ItensTalkForm(forms.ModelForm):
+    class Meta:
+        model = ItensTalk
+        fields = ["talk"]
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fields["talk"].queryset = Talk.objects.exclude(pk__in=Course.objects.values_list('pk', flat=True))
+        self.fields["talk"].queryset = Talk.just_talk()
+
+
+TalkFormSet = inlineformset_factory(
+    Registration,
+    ItensTalk,
+    form=ItensTalkForm,
+    fields=("talk", "name_speaker", "start_time"),
+    extra=1,
+)
+
+
+class ItensCourseForm(forms.ModelForm):
+    class Meta:
+        model = ItensCourse
+        fields = ["course", "name_speaker", "start_time"]
+
+
+CourseFormSet = inlineformset_factory(
+    Registration,
+    ItensCourse,
+    form=ItensCourseForm,
+    fields=("course", "name_speaker", "start_time"),
+    extra=1,
+)
