2789932a3ae783d43df787a6f1c7027b167d45ea
AlissonSH <alisson_sielo@hotmail.com>
2025-02-13 22:06:58 -0300

    Update: Ajuste do javascript antigo para o javascript es6 no arquivo de student

--- eventex/registration/static/registration/js/student.js
+++ eventex/registration/static/registration/js/student.js
@@ -1,33 +1,57 @@
-$(document).ready(function () {
-    $(".form_select2").select2({
-        placeholder: "",
-        allowClear: true,
-        width: '100%'
+document.addEventListener('DOMContentLoaded', () => {
+    const elementsSelect2 = document.querySelectorAll(".form_select2");
+
+    elementsSelect2.forEach((elem) => {
+        $(elem).select2({
+            placeholder: "",
+            allowClear: true,
+            width: '100%'
+        });
     });
 
-    $("#id_student").select2({
+    const elementStudent = document.querySelector("#id_student");
+    $(elementStudent).select2({
         ajax: {
             delay: 500,
             url: URL_LISTA_STUDENT_AJAX,
             dataType: 'json'
         }
     });
-    $("#id_student").on('change', function () {
-        getDadosStudent();
+
+    const cpfId = document.getElementById("id_cpf");
+    const phoneId = document.getElementById("id_phone");
+    if (!elementStudent.value) {
+        cpfId.value = "";
+        phoneId.value = "";
+    }
+
+    $(elementStudent).on('change', async function(){
+        await getDadosStudent();
     });
 });
 
-const getDadosStudent = () => {
-    axios.get(URL_DADOS_STUDENT_AXIOS, {
-        params: {
-            "id": $("#id_student").find('option:selected').val()
+async function getDadosStudent() {
+    const studentId = document.querySelector("#id_student")?.value;
+    try {
+        const response = await axios.get(URL_DADOS_STUDENT_AXIOS, {
+            params: {
+                id: studentId,
+            }
+        });
+        const {cpf, phone} = response.data
+        const cpfValue = document.getElementById("id_cpf");
+        const phoneValue = document.getElementById("id_phone");
+        if (cpfValue) {
+            cpfValue.value = cpf;
+            cpfValue.classList.add("mask_cpf");
+            cpfValue.dispatchEvent(new Event("input"));
         }
-    })
-    .then((response) => {
-        $("#id_cpf").val(response.data.cpf).addClass('mask_cpf').trigger('input');
-        $("#id_phone").val(response.data.phone).addClass('mask_phone').trigger('input');
-    })
-    .catch((error) => {
-        console.log("Erro ao carregar o estudante." + error);
-    })
+        if (phoneValue) {
+            phoneValue.value = phone;
+            phoneValue.classList.add("mask_phone");
+            phoneValue.dispatchEvent(new Event("input"))
+        }
+    } catch (error) {
+        console.error("Erro ao carregar o estudante.", error);
+    }
 }
